1b49af76fc1b48e82aabe1fb0899f73e522cf1c2
MARKENVY-HP\cgmmr <mark.h.mackey@gmail.com>
2018-08-25 22:47:38 -0700

    Getting icons to work

---
+++ js/practiceDOM.js
@@ -0,0 +1,496 @@
+const weatherData = {
+    "message": "accurate",
+    "cod": "200",
+    "count": 10,
+    "list": [{
+            "id": 5377100,
+            "name": "Nipomo",
+            "coord": {
+                "lat": 35.0428,
+                "lon": -120.476
+            },
+            "main": {
+                "temp": 58.24,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 55.4,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 3.49,
+                "deg": 338.504
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5324802,
+            "name": "Arroyo Grande",
+            "coord": {
+                "lat": 35.1186,
+                "lon": -120.5908
+            },
+            "main": {
+                "temp": 58.62,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 57.2,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5354552,
+            "name": "Grover Beach",
+            "coord": {
+                "lat": 35.1216,
+                "lon": -120.6213
+            },
+            "main": {
+                "temp": 58.62,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 57.2,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5378763,
+            "name": "Oceano",
+            "coord": {
+                "lat": 35.0989,
+                "lon": -120.6124
+            },
+            "main": {
+                "temp": 58.62,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 57.2,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5383431,
+            "name": "Pismo Beach",
+            "coord": {
+                "lat": 35.1428,
+                "lon": -120.6413
+            },
+            "main": {
+                "temp": 58.62,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 57.2,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5332963,
+            "name": "Avila Beach",
+            "coord": {
+                "lat": 35.053,
+                "lon": -120.5963
+            },
+            "main": {
+                "temp": 58.06,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 55.4,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5392329,
+            "name": "Paso Robles",
+            "coord": {
+                "lat": 35.3666,
+                "lon": -120.5344
+            },
+            "main": {
+                "temp": 98.84,
+                "pressure": 1014,
+                "humidity": 88,
+                "temp_min": 78.2,
+                "temp_max": 99.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5392323,
+            "name": "San Luis Obispo",
+            "coord": {
+                "lat": 35.2827,
+                "lon": -120.6597
+            },
+            "main": {
+                "temp": 58.53,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 57.2,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5372954,
+            "name": "Morro Bay",
+            "coord": {
+                "lat": 35.1855,
+                "lon": -120.7033
+            },
+            "main": {
+                "temp": 58.62,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 57.2,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 5.82,
+                "deg": 130
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        },
+        {
+            "id": 5393180,
+            "name": "Santa Maria",
+            "coord": {
+                "lat": 34.953,
+                "lon": -120.4358
+            },
+            "main": {
+                "temp": 58.24,
+                "pressure": 1014,
+                "humidity": 87,
+                "temp_min": 55.4,
+                "temp_max": 60.8
+            },
+            "dt": 1534258560,
+            "wind": {
+                "speed": 3.49,
+                "deg": 338.504
+            },
+            "sys": {
+                "country": ""
+            },
+            "rain": null,
+            "snow": null,
+            "clouds": {
+                "all": 90
+            },
+            "weather": [{
+                "id": 701,
+                "main": "Mist",
+                "description": "mist",
+                "icon": "50d"
+            }]
+        }
+    ]
+};
+
+function getCityData(city) {
+    let cityIdName;
+    let cityDiv;
+    let temperature;
+    let cityTemperature;
+    let humidity;
+    let cityHumidity;
+    let pressure;
+    let cityPressure;
+    let tempMin;
+    let cityTempMin;
+    let tempMax;
+    let cityTempMax;
+    let windSpeed;
+    let cityWindSpeed;
+    let windDirection;
+    let cityWindDirection;
+    let rainLevel;
+    let cityRainLevel;
+    let snowLevel;
+    let citySnowLevel;
+
+    for (let key in weatherData) {
+        //access the keys
+        for (let i = 0; i < weatherData.list.length; i++) {
+            //accessing List Array, list of objects
+            let listData = weatherData.list[i];
+            //access individual City data
+            // if (listData.name === city) {
+            //     return (listData);
+            // }
+
+            //ADD CITY NAME
+            cityIdName = "cityName" + i;
+            cityDiv = document.getElementById(cityIdName);
+            cityDiv.innerHTML = listData.name;
+
+            //UPDATE FOOTER
+            lastUpdated = document.getElementById("LastUpdated");
+            let d = new Date();
+
+            lastUpdated.innerHTML = "Last updated on " + (d.getMonth()+1)+"/"+d.getDate()+"/"+d.getFullYear() + " at " + d.getHours()+":"+d.getMinutes();
+        }
+         //ADD TEMPERATURE TO CITY TABLE
+         temperature = document.createElement("p");
+         temperature.className = "temperature";
+         cityTemperature = document.createTextNode("This is a paragraph");
+         temperature.appendChild(cityTemperature);
+         document.getElementById("cityTable"+i).appendChild(temperature);
+         temperature.innerHTML = "Temperature: " + listData.main.temp + "℉";
+
+         // //ADD HUMIDITY TO CITY TABLE
+         humidity = document.createElement("p");
+         humidity.className = "humidity";
+         cityHumidity = document.createTextNode("This is a paragraph");
+         humidity.appendChild(cityHumidity);
+         document.getElementById("cityTable"+i).appendChild(humidity);
+         humidity.innerHTML = "Humidity: " + listData.main.humidity + "%";
+
+         // //ADD PRESSURE TO CITY TABLE
+         pressure = document.createElement("p");
+         pressure.className = "pressure";
+         cityPressure = document.createTextNode("This is a paragraph");
+         pressure.appendChild(cityPressure);
+         document.getElementById("cityTable"+i).appendChild(pressure);
+         pressure.innerHTML = "Pressure: " + Math.round((listData.main.pressure / 33.8639) * 100) / 100 + " inHg";
+
+         // //ADD TEMP MIN TO CITY TABLE
+         tempMin = document.createElement("p");
+         tempMin.className = "tempMin";
+         cityTempMin = document.createTextNode("This is a paragraph");
+         tempMin.appendChild(cityTempMin);
+         document.getElementById("cityTable"+i).appendChild(tempMin);
+         tempMin.innerHTML = "Minimum Temperature: " + listData.main.temp_min + "℉";
+
+         // //ADD TEMP MAX TO CITY TABLE
+         tempMax = document.createElement("p");
+         tempMax.className = "tempMax";
+         cityTempMax = document.createTextNode("This is a paragraph");
+         tempMax.appendChild(cityTempMax);
+         document.getElementById("cityTable"+i).appendChild(tempMax);
+         tempMax.innerHTML = "Maximum Temperature: " + listData.main.temp_max + "℉";
+
+         // //ADD WIND SPEED TO CITY TABLE
+         windSpeed = document.createElement("p");
+         windSpeed.className = "windSpeed";
+         cityWindSpeed = document.createTextNode("This is a paragraph");
+         windSpeed.appendChild(cityWindSpeed);
+         document.getElementById("cityTable"+i).appendChild(windSpeed);
+         windSpeed.innerHTML = "Wind Speed: " + listData.wind.speed + "mph";
+
+         // //ADD WIND DIRECTION TO CITY TABLE
+         windDirection = document.createElement("p");
+         windDirection.className = "windDirection";
+         cityWindDirection = document.createTextNode("This is a paragraph");
+         windDirection.appendChild(cityWindDirection);
+         document.getElementById("cityTable"+i).appendChild(windDirection);
+         // round to nearest integer
+         roundedWindDirection = Math.round(listData.wind.deg);
+         //determine wind direction
+         if (roundedWindDirection <= 22) {
+             windSpeed.innerHTML = "Wind Direction: N";
+         } else if (roundedWindDirection <= 67) {
+             windSpeed.innerHTML = "Wind Direction: NE";
+         } else if (roundedWindDirection <= 112) {
+             windSpeed.innerHTML = "Wind Direction: E";
+         } else if (roundedWindDirection <= 157) {
+             windSpeed.innerHTML = "Wind Direction: SE";
+         } else if (roundedWindDirection <= 202) {
+             windSpeed.innerHTML = "Wind Direction: S";
+         } else if (roundedWindDirection <= 247) {
+             windSpeed.innerHTML = "Wind Direction: SW";
+         } else if (roundedWindDirection <= 292) {
+             windSpeed.innerHTML = "Wind Direction: W";
+         } else if (roundedWindDirection <= 337) {
+             windSpeed.innerHTML = "Wind Direction: NW";
+         } else if (roundedWindDirection <= 360) {
+             windSpeed.innerHTML = "Wind Direction: N";
+         }
+
+         // //ADD RAIN TO CITY TABLE
+         rainLevel = document.createElement("p");
+         rainLevel.className = "rainLevel";
+         cityRainLevel = document.createTextNode("This is a paragraph");
+         rainLevel.appendChild(cityRainLevel);
+         document.getElementById("cityTable"+i).appendChild(rainLevel);
+         if (listData.rain === null) {
+             rainLevel.innerHTML = "Rain: " + "0.0" + " inches";
+         } else {
+             rainLevel.innerHTML = "Rain: " + listData.rain + " inches";
+         }
+
+         // //ADD SNOW TO CITY TABLE
+         snowLevel = document.createElement("p");
+         snowLevel.className = "snowLevel";
+         citySnowLevel = document.createTextNode("This is a paragraph");
+         snowLevel.appendChild(citySnowLevel);
+         document.getElementById("cityTable"+i).appendChild(snowLevel);
+         if (listData.snow === null) {
+             snowLevel.innerHTML = "Snow: " + "0.0" + " inches";
+         } else {
+             snowLevel.innerHTML = "Snow: " + listData.snow + " inches";
+         }
+    }
+}
+
+let cityData = getCityData("San Luis Obispo");
+
+//console.log(cityData);
